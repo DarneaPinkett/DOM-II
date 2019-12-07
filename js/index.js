688fc568d7b08ced807c086b4687ea48537fa084
DarneaPinkett <56055954+DarneaPinkett@users.noreply.github.com>
2019-12-07 12:51:32 -0500

    P3

--- js/index.js
+++ js/index.js
@@ -14,3 +14,23 @@ header.addEventListener('mouseover', (event) => {
     event.target.style.fontWeight = 'bold'
     event.target.style.color = 'red'
 })
+
+// mouse enter
+const footerText = document.querySelector('.footer p');
+footerText.addEventListener('mouseenter', function () {
+    footerText.style.color = 'crimson'
+})
+
+// key up
+const myBtn = document.querySelector('body');
+myBtn.addEventListener('keyup', function () {
+    body.style.color = "dodgerblue"
+})
+
+// click
+const myBtns = document.querySelectorAll(".btn");
+myBtns.forEach(button => {
+    button.addEventListener('click', e => {
+        e.target.style.border = '3px solid black';
+    })
+})
