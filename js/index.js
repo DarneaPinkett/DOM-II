278c3e6cf6092e8bc4b10b53075b0cb4ef1b77ce
DarneaPinkett <56055954+DarneaPinkett@users.noreply.github.com>
2019-12-07 09:39:02 -0500

    2Events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+
+// keypress
+const body = document.querySelector('body');
+body.addEventListener('keypress', (e) => {
+    e.target.style.backgroundColor = "lavender"
+})
+
+
+// Mouseover
+const header = document.querySelector('.main-navigation');
+header.addEventListener('mouseover', (event) => {
+    event.stopPropagation();
+    event.target.style.fontWeight = 'bold'
+    event.target.style.color = 'red'
+})
